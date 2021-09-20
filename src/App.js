c463cd343ea9d29541cf3a2a9730cc614e433eb5
abhilash <abhilash.kamble376@gmail.com>
2021-09-20 21:54:13 +0530

    upadfte

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Routes from './routes.js'
 import Header from './components/Header';
+import { MyChart } from './chartjs.js';
 
 function App() {
   return (
     <div>
       <Routes />
-
+      <MyChart />
     </div>
   )
 }
